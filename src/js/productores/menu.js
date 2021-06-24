9d328a09bb32114cc6f02fb76731bcf0c54f484a
Diana_Londoño <dianaclondonoc@gmail.com>
2021-06-24 11:38:57 -0500

    Hice cambios en el JS

--- src/js/productores/menu.js
+++ src/js/productores/menu.js
@@ -2,9 +2,9 @@ function inicio(){
     let principal= document.getElementsByClassName('ContainerHome')[0];
 	principal.style.display= "flex";
 	let segundo= document.getElementsByClassName("Quien")[0];
-	segundo.style.display="none";
-	let tercero= document.getElementsByClassName("products")[0];
-	tercero.style.display="none";
+	segundo.style.display="flex";
+	let tercero= document.getElementsByClassName("ContainerProductos")[0];
+	tercero.style.display="flex";
 }
 
 function quienes(){
@@ -12,12 +12,12 @@ function quienes(){
 	principal.style.display="flex";
     let segundo= document.getElementsByClassName('ContainerHome')[0];
 	segundo.style.display= "none";     
-	let tercero= document.getElementsByClassName("products")[0];
+	let tercero= document.getElementsByClassName("ContainerProductos")[0];
 	tercero.style.display="none";
 }
 
 function productos(){
-    let principal= document.getElementsByClassName("products")[0];
+    let principal= document.getElementsByClassName("ContainerProductos")[0];
 	principal.style.display="flex";
     let segundo= document.getElementsByClassName('ContainerHome')[0];
 	segundo.style.display= "none";
